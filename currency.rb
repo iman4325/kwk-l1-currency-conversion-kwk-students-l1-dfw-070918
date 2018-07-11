27f6dea7deae6fd78aec956be0c28c815c317c2e
iman4325 <imantanzeem@hotmail.com>
2018-07-11 14:09:37 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,10 +1,24 @@
 # Write your cod here
-def usd_to_eur(dollar_amount)
-  dollar_amount = 1.00
-
-  
+def usd_to_eur(amount)
+  new_currency = amount * 0.86
 end
 
-def eur_to_usd
-  
+def eur_to_usd(amount)
+  new_currency = amount * 0.86
 end 
+
+def usd_to_jpy(amount)
+  new_currency = amount * 0.86
+end 
+
+def jpy_to_usdZ(amount)
+  new_currency = amount * 0.86
+end
+
+def gbp_to_usd(amount)
+  new_currency = amount * 0.86
+end 
+
+def aud_to_usd(amount)
+  new_currency = amount * 0.86
+end
